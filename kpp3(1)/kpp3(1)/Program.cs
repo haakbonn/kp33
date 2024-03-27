c304050aeb55f2964b6abb3834f65f10e090a24d
haakbonn <143547066+haakbonn@users.noreply.github.com>
2024-03-27 08:46:47 +0200

    Add files via upload

---
+++ kpp3(1)/kpp3(1)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace kpp3_1_
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArray dynamicArray = new DynamicArray(10);
+            dynamicArray.FillRandom();
+            dynamicArray.PrintArray();
+
+            string filePath = "dynamic_array.json";
+            dynamicArray.SaveToJson(filePath);
+
+            DynamicArray loadedDynamicArray = DynamicArray.LoadFromJson(filePath);
+            loadedDynamicArray.PrintArray();
+            Console.ReadLine();
+        }
+    }
+}
